d7d481c7c2888deabf76e00c537ad47325d48f0e
digao00 <joaorodrigogame@gmail.com>
2023-10-26 17:49:54 -0400

    melhorias

--- script.js
+++ script.js
@@ -2,44 +2,25 @@ document.getElementById("login-form").addEventListener("submit", function(event)
     event.preventDefault();
     const usuario = document.getElementById("usuario").value;
     const senha = document.getElementById("senha").value;
-    if (usuario === "teste" && senha === "teste") {
+    if (usuario === "t" && senha === "t") {
         alert("Login bem-sucedido!");
 
         const loginForm = document.getElementById("login-form");
-        loginForm.innerHTML = `<h1>Bem-vindo à Página de Downloads</h1>
-            <p>Aqui você pode baixar os arquivos desejados:</p>
-            <ul>
-            <center>
-                <li>
-                    <a href="imagem.png" download>
-                        <span class="text-center">Imagem 1</span>
-                    </a>
-                    <div class="text-center">
-                        <button class="btn btn-primary" onclick="previewImage('imagem.png')">Ver Imagem</button>
-                    </div>
-                </li>
-                <li>
-                    <a href="documento.pdf" download>
-                        <span class="text-center">Documento PDF</span>
-                    </a>
-                    <div class="text-center">
-                        <button class="btn btn-primary" onclick="downloadFile('documento.pdf')">Download</button>
-                    </div>
-                </li>
-                </center>
-                <!-- Adicione links para outros arquivos -->
-            </ul>`;
+        loginForm.style.display = "none";
+        const Container = document.getElementById("container");
+        Container.style.display = "none";
+
+        const container2 = document.getElementById("container2");
+        container2.style.display = "block";
     } else {
         alert("Credenciais inválidas. Tente novamente.");
     }
 });
 
 function previewImage(imageUrl) {
+    // Lógica para visualização de imagem
 }
 
-
-
 function downloadFile(fileUrl) {
-    // Código para iniciar o download do arquivo
-    // Substitua pelo seu código de download de arquivo
+    // Lógica para download de arquivo
 }
