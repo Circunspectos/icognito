386d97512ea2bdb40d72ac502b2bd2873cc8563f
Caio Hickmann <caio.hickmann@estudante.ifms.edu.br>
2023-10-27 14:05:31 -0400

    atualizações

--- upload.php
+++ upload.php
@@ -1,6 +1,6 @@
 <?php
-if (isset($_POST['submit'])) {
-    $uploadDirectory = 'uploads/';
+if (isset($_FILES['file'])) {
+    $uploadDirectory = '/';
 
     if (!file_exists($uploadDirectory)) {
         mkdir($uploadDirectory, 0777, true);
